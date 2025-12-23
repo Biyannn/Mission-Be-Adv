c5815296feefb930b068be933dba9bcb6f12c7aa
Biyannn <ayyubian07@gmail.com>
2025-12-23 14:49:55 +0700

    fix:menghapus .env dari github

--- project/db/connection.js
+++ project/db/connection.js
@@ -6,7 +6,10 @@ const db = mysql.createPool({
   user: process.env.MYSQLUSER || process.env.DB_USER,
   password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
   database: process.env.MYSQLDATABASE || process.env.DB_NAME,
-  port: process.env.MYSQLPORT || process.env.PORT || 3306,
+  port: process.env.MYSQLPORT || process.env.DB_PORT || 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 module.exports = db;
